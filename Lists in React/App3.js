f15fbe4cf8c8888ec19756a88fb379f9d6fb9075
roseestacion <116148564+roseestacion@users.noreply.github.com>
2022-10-29 14:13:16 +0800

    Create App3.js

---
+++ Lists in React/App3.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      ...
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return <li>{item.title}</li>;
+         })}
+      </ul>
+    </div>
+  );
+}
