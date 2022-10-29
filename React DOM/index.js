22cebe7032bd9f72e1d56c861736b645c935424e
roseestacion <116148564+roseestacion@users.noreply.github.com>
2022-10-29 14:23:55 +0800

    Create index.js

---
+++ React DOM/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <h1>Hello React World</h1>,
+  document.getElementById('root')
+);
